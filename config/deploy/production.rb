4a1dd913c0627fc2291cda7d51f7c7e1648bb6e1
papayera <fiestaycumbia@gmail.com>
2017-06-28 03:22:54 -0500

    ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -20,9 +20,9 @@
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
-role :app, %w{deploy@192.168.1.3}
-role :web, %w{deploy@192.168.1.3}
-role :db,  %w{deploy@192.168.1.3}
+role :app, %w{deploy@192.168.1.1}
+role :web, %w{deploy@192.168.1.1}
+role :db,  %w{deploy@192.168.1.1}
 
 
 
