bf8adfff896b962f4009bf22ab2e73a219c89dd0
papayera <fiestaycumbia@gmail.com>
2017-07-01 16:33:38 -0500

    push

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = "/opt/www/testapp/current"
+root = "/opt/www/papayera/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.testapp.sock"
+listen "/tmp/unicorn.papayera.sock"
 worker_processes 1
 timeout 30
